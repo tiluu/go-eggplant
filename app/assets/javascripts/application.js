87ad78b888dae3111c6c1749968bf0524911192a
tina <tina.p.luu@gmail.com>
2015-11-23 19:57:00 -0800

    toggle list

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+(function($){
+
+	//Toggle list
+	$('.trip--fav_action').find('button').on('click', function(){
+		$('.trip--fav_action').find('.btn').removeClass('active');
+		$(this).addClass('active');
+
+		$('.trip--list').find('ul').addClass('closed');
+		$('.trip--list').find('[data-type="'+$(this).data('type')+'"]').removeClass('closed');
+
+	});
+
+
+})(jQuery, this, this.document);
