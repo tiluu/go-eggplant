9010f2687872fe818afc2bbef82be13c20c563b6
tina <tina.p.luu@gmail.com>
2015-11-24 18:28:09 -0800

    bring in styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require angular
 //= require angular-resource
 //= require_tree .
+
+(function($){
+
+	//Toggle list
+	$('.trip--fav_action').find('button').on('click', function(){
+		$('.trip--fav_action').find('.btn').removeClass('active');
+		$(this).addClass('active');
+
+		$('.trip--list').find('ul').addClass('closed');
+		$('.trip--list').find('[data-type="'+$(this).data('type')+'"]').removeClass('closed');
+
+	});
+
+
+})(jQuery, this, this.document);
