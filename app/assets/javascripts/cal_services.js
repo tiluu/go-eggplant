543e1bc1e56554b6f0c3bb4e5d202334a359109a
carmen <c_chow568@yahoo.ca>
2015-11-27 10:49:56 -0800

    Refactor calendar.js

--- app/assets/javascripts/cal_services.js
+++ app/assets/javascripts/cal_services.js
@@ -3,16 +3,18 @@
 
     services.factory("mnthService", function() {
         //todo: account for leap year
-        var months = {January: {num: 1, num_days: 31, first_week: []}, 
-                      February: {num: 2, num_days: 28, first_week: []}, 
-                      March: {num: 3, num_days: 31, first_week: []}, 
-                      April: {num: 4, num_days: 30, first_week: []},
-                      May: {num: 5, num_days: 31, first_week: []},                                                                         June: {num: 6, num_days: 30, first_week: []}, 
-                      July: {num: 7, num_days: 31, first_week: []},
-                      August: {num: 8, num_days: 31, first_week: []}, 
-                      September: {num: 9, num_days: 30, first_week: []},
-                      October: {num: 10, num_days: 31, first_week: []},                                                                    November: {num: 11, num_days: 30, first_week: []},
-                      December: {num: 12, num_days: 31, first_week: []}
+        var months = {January: {num: 1, num_days: 31}, 
+                      February: {num: 2, num_days: 28}, 
+                      March: {num: 3, num_days: 31}, 
+                      April: {num: 4, num_days: 30},
+                      May: {num: 5, num_days: 31},
+                      June: {num: 6, num_days: 30}, 
+                      July: {num: 7, num_days: 31},
+                      August: {num: 8, num_days: 31}, 
+                      September: {num: 9, num_days: 30},
+                      October: {num: 10, num_days: 31},
+                      November: {num: 11, num_days: 30},
+                      December: {num: 12, num_days: 31}
                      };
         return months;    
     });
@@ -24,9 +26,19 @@
     
     services.factory("tripData", function() {
         var div = document.getElementById('date-data');
+        var start_date = div.getAttribute("start_date");
+        var end_date = div.getAttribute("end_date")
+        var start = new Date(start_date);
+        var end = new Date(end_date);
+
         var data = {
-            start_d: div.getAttribute("start_date"),
-            end_d: div.getAttribute("end_date")
+            start_y: start.getFullYear(),
+            start_m: start.getMonth(),
+            start_d: start.getDate(),
+            end_y: end.getFullYear(),
+            end_m: end.getMonth(),
+            end_d: end.getDate()
+               
         };
         return data;
     });
--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,40 +2,24 @@
     var calendar = angular.module('Calendar', ['calServices']);
 
    
-    calendar.controller('CalendarCtrl', function($scope, mnthService, wkService, tripData) {
-        $scope.data = tripData;     
+    calendar.controller('CalendarCtrl', function($scope, mnthService, wkService, tripData) {  
         $scope.weekdays = wkService;
+        var tripDates = tripData; 
+        var months = Object.keys(mnthService);      
 
-        var months = Object.keys(mnthService);
-        var start_d = new Date($scope.data.start_d)
-        var end_d = new Date($scope.data.end_d);
-
-        $scope.year = start_d.getFullYear();
         // convert date.getMonth() to name of current month 
-        
         $scope.getMonths = function() {
+                                   
             var trip_months = [];
-            var m1 = start_d.getMonth();
-            var m2 = end_d.getMonth();
-            var diff = m2 - m1;
-            // need to account for year transitions
-            if (diff > 0) { 
-                for (var m = m1; m <= m2; m++) {
+            var m1 = tripDates.start_m;
+            var m2 = tripDates.end_m;
+            
+            for (var m = m1; m <= m2; m++) {                      
                 trip_months.push(months[m]);            
-                }    
-            }
-            else {
-                trip_months.push(months[m1])
-            }
+              }
+
             return trip_months;    
-        };
-       
-/**
-        $scope.getMonth = function() {
-            var m = start_d.getMonth();
-            return months[m];
-        }
-**/
+        };  
 
         // calculate current date
         $scope.getDay = function(day, week) {
@@ -49,11 +33,13 @@
 
         // find out what day of the week the first day of the current month falls on
         $scope.firstDayOfMonth = function(month) {
-            var year = $scope.year.toString();
+            var start_y = tripDates.start_y;
+            var end_y = tripDates.end_y;
             var start_weekdays = [];
            
-            for (var key in months) {
-                var first_day_of_month = months[key]+" 1 "+year;
+            for (var mnth in months) {
+                //to account for year, may consider making year a param for the function 
+                var first_day_of_month = months[mnth]+" 1 "+ start_y;
                 var d = new Date(first_day_of_month);
                 start_weekdays.push(d.getDay()); 
             }; 
@@ -66,20 +52,21 @@
             // var day keeps track  of # days before 1st day of month
             var day = 0;
             var start_day = $scope.firstDayOfMonth(month);
-            var first_row = mnthService[month].first_week; 
+            var m = mnthService[month];
+            m.first_week = []; 
                 
-            while (day < start_day && first_row.length < start_day) {
-                first_row.push('');
+            while (day < start_day && m.first_week.length < start_day) {
+                m.first_week.push('');
                 day++;
             }
                 
             var first_weekdays = 1;
-            while (first_row.length < 7) {
-                first_row.push(first_weekdays);
+            while (m.first_week.length < 7) {
+                m.first_week.push(first_weekdays);
                 first_weekdays++;
             }               
             
-            return first_row;
+            return m.first_week;
         };
        
        // build the rest of the calendar 
@@ -97,20 +84,20 @@
 
        // highlights trip date
        $scope.tripDates = function(month, cal_day) {
-           var start_day = start_d.getDate();
-           var start_mnth = start_d.getMonth() + 1;
-           var end_day = end_d.getDate();
-           var end_mnth = end_d.getMonth() + 1;
+           var start_d = tripDates.start_d;
+           var start_m = tripDates.start_m + 1;
+           var end_d = tripDates.end_d;
+           var end_m = tripDates.end_m + 1;
            
            var curr_month = mnthService[month].num; 
-           var start_diff = curr_month - start_mnth;
-           var end_diff = end_mnth - curr_month;
-           //  new_end_d changes as ng-repeat loop thru each month
-           var new_start_d = start_day - (start_day * start_diff)
-           var new_end_d = end_day + (mnthService[month].num_days * end_diff)
+           var start_diff = curr_month - start_m;
+           var end_diff = end_m - curr_month;
            
-           var match_days = new_start_d <= cal_day && new_end_d >= cal_day;
-           console.log(month + new_start_d + " to " + new_end_d + " " + cal_day + match_days);
+           var new_start_d = start_d - (start_d * start_diff)
+           var new_end_d = end_d + (mnthService[month].num_days * end_diff)
+           
+           var match_days = new_start_d <= cal_day && new_end_d >= cal_day && cal_day !== '';
+       
            return match_days;
        };
 
