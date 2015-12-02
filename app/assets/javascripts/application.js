ec8c42908a983778a8f772e3f6d5b22a6181b541
tina <tina.p.luu@gmail.com>
2015-12-01 18:06:04 -0800

    fixed issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,5 +28,18 @@
 
 	});
 
+	var app = angular.module('gemStore', []);
+
+	app.controller("TabController", function(){
+		// this.tab = 1;
+		// this.setTab = function(newValue){
+  //     this.tab = newValue;
+  //   };
+
+  //   this.isSet = function(tabName){
+  //     return this.tab === tabName;
+  //   };
+	});
+
 
 })(jQuery, this, this.document);
