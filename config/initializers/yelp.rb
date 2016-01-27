9041e2180de95d360ebca0f322fada14e32150d3
car <c_chow568@yahoo.ca>
2016-01-26 18:30:10 -0800

    Configured app for heroku

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -6,4 +6,12 @@ if !Rails.env.test?
 	    config.token = s.YELP_TOKEN
 	    config.token_secret = s.YELP_TOKEN_SECRET
 	end
+
+elsif Rails.env.production? 
+    Yelp.client.configure do |config|
+      config.consumer_key = ENV['YELP_CONSUMER_KEY']
+      config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+      config.token = ENV['YELP_TOKEN']
+      config.token_secret = ENV['YELP_TOKEN_SECRET']
+    end
 end
